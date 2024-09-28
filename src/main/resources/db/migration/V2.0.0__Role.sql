91f5744fe07ba72f193c6a1391d7d712e6160258
mazhar <mazhar1322@cseku.ac.bd>
2024-09-29 00:14:35 +0600

    enable flyway

--- src/main/resources/db/migration/V2.0.0__Role.sql
+++ src/main/resources/db/migration/V2.0.0__Role.sql
@@ -1,3 +1,9 @@
+CREATE TABLE roles
+(
+    id   BIGINT NOT NULL,
+    name VARCHAR(255),
+    CONSTRAINT pk_roles PRIMARY KEY (id)
+);
 insert into roles (id, name) values(1, 'Admin');
 insert into roles (id, name) values(2, 'User');
 insert into roles (id, name) values(3, 'Manager');
