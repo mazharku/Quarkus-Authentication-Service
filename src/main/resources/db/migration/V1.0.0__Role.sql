729dd4070e46d2d5a6a6ef68323df980ccf8bbd7
MAZHAR <mazhar@tiller.com.bd>
2024-09-29 09:36:11 +0600

    fix migration scripts order

--- src/main/resources/db/migration/V1.0.0__Role.sql
+++ src/main/resources/db/migration/V1.0.0__Role.sql
@@ -7,4 +7,3 @@ CREATE TABLE roles
 insert into roles (id, name) values(1, 'Admin');
 insert into roles (id, name) values(2, 'User');
 insert into roles (id, name) values(3, 'Manager');
-alter sequence myentity_seq restart with 4;
